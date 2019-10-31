6b0b9824f06cac182177833c3df20e2099588474
Mikoláš Stuchlík <stuchlej@gmail.com>
2019-10-31 17:17:38 +0100

    Zakládající přispění

---
+++ skautis-material/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  skautis-material
+//
+//  Created by Mikoláš Stuchlík on 31/10/2019.
+//  Copyright © 2019 Mikoláš Stuchlík. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ skautis-materialTests/skautis_materialTests.swift
@@ -0,0 +1,34 @@
+//
+//  skautis_materialTests.swift
+//  skautis-materialTests
+//
+//  Created by Mikoláš Stuchlík on 31/10/2019.
+//  Copyright © 2019 Mikoláš Stuchlík. All rights reserved.
+//
+
+import XCTest
+@testable import skautis_material
+
+class skautis_materialTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
